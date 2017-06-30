a31912a65ee8d5902951d5cf80b12204b2970eee
digkratos <diogomoura10@gmail.com>
2017-06-30 10:06:45 -0300

    tela de login

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -3,17 +3,29 @@
     <head>
         <title><?php echo $title; ?></title>
         <meta charset="UTF-8" />
-        <link type="text/css" rel="stylesheet" href="<?php echo base_url(); ?>assets/bootstrap/css/estilo.css"/>
+        <link type="text/css" rel="stylesheet" href="<?php echo base_url(); ?>assets/css/global.css"/>
+        <link type="text/css" rel="stylesheet" href="<?php echo base_url(); ?>assets/css/login.css"/>
         <link type="text/css" rel="stylesheet" href="<?php echo base_url(); ?>assets/bootstrap/css/bootstrap.min.css"/>
-        <script  src="<?php echo base_url();?>assets/bootstrap/js/bootstrap.min.js"></script>
-        <script  src="<?php echo base_url(); ?>assets/bootstrap/js/bootstrap.min.js"></script>
-        <script  src="<?php echo base_url(); ?>assets/bootstrap/js/sigere.js"></script>
+        <script  src="<?php echo base_url(); ?>assets/js/jquery-3.2.1.min  .js"></script>
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+        <script  src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
+        <script  src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
+        <script  src="<?php echo base_url(); ?>assets/js/sigere.js"></script>
+       
+
+        
 
 
     </head>
-    <body
-            
-        
-      
+    <body>
+
+        <div class="header">
+            <div class="title">SIGERE</div>
+        </div>
+        <div class="section">
+            <div class="login"></div>
+        </div>
+        <div class="footer"></div>
     </body>
-</html>
+
+
