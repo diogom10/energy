b338de3d4fd6fd7f5d62afb8601af54087f7eb4e
digkratos <diogomoura10@gmail.com>
2017-06-30 12:36:21 -0300

    almoço

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -3,17 +3,18 @@
     <head>
         <title><?php echo $title; ?></title>
         <meta charset="UTF-8" />
-        <link type="text/css" rel="stylesheet" href="<?php echo base_url(); ?>assets/css/global.css"/>
+
         <link type="text/css" rel="stylesheet" href="<?php echo base_url(); ?>assets/css/login.css"/>
         <link type="text/css" rel="stylesheet" href="<?php echo base_url(); ?>assets/bootstrap/css/bootstrap.min.css"/>
         <script  src="<?php echo base_url(); ?>assets/js/jquery-3.2.1.min  .js"></script>
         <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
         <script  src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
         <script  src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
         <script  src="<?php echo base_url(); ?>assets/js/sigere.js"></script>
-       
 
-        
+
+
 
 
     </head>
@@ -23,7 +24,21 @@
             <div class="title">SIGERE</div>
         </div>
         <div class="section">
-            <div class="login"></div>
+            <div class="login">
+                <div class="p_corpo_inputs">
+                    <div class="title_login">Faça seu Login</div>
+                    <div class="p_inputs title_inputs">
+                        <label>Email: </label>
+                        <input type="email" class="ipt-email" >
+                        <br/><br/>
+                        <label>Senha:</label>
+                        <input type="password" class="ipt-password">
+                        <br/>
+                        <div class="btn">Login</div>
+                        <div class="btn">Cadastro</div>
+                    </div>
+                </div>
+            </div>
         </div>
         <div class="footer"></div>
     </body>
